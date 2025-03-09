0f14d72897af14165d3cf138dc2861a3db90a8b1
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-09 19:48:43 +0500

    math methods done

---
+++ conditions.py
@@ -0,0 +1,30 @@
+# if = Do some code only IF some cond is true
+# in the other case do smth else
+#
+# age = int(input('Enter your age: '))
+# if age>=100:
+#     print('You are too old to sign up')
+# elif  age >= 18 :
+#     print('You are now signed up')
+# elif age < 0:
+#      print('You have not been born yet!')
+# elif age<18:
+#     print('You must be 18+ to sign up!')
+#
+# response = input('Would you like food? (Y/N): ')
+# if response == 'Y':
+#     print('Have some food')
+# else:
+#     print('no food for you')
+
+# name = input('Enter your name: ')
+# if name == "":
+#     print('You did not type in your name')
+# else:
+#     print(f'Hello, {name}')
+# for_sale = True
+# if for_sale:
+#     print('This item is for sale')
+# else:
+#     print('This item is not for sale!')
+
