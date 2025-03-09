f9742ddf70ae631665a9d7e20c1bf79ec7947a5a
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-09 20:53:49 +0500

    to be continued ...

---
+++ logical_operations.py
@@ -0,0 +1,6 @@
+# logical operators = evaluate multiple conditions (or, and, not)
+#                     or = at least one cond must be True
+#                     and = both conditions must be True
+#                     not = inverts the condition (not False, not True)
+
+
