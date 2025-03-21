6cef3959c851113b6eecbfc00ea8f6052012394d
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-21 11:49:00 +0500

    skyscraper EGOI activity done!

---
+++ egoi1/mall.py
@@ -0,0 +1,6 @@
+import math
+def time_calculator():
+    x = math.fabs(float(input('Enter the distance between mall and house(whole numbers): ')))
+    t = 2*(x/1)
+    return t
+print(time_calculator())
