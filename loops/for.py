f656acbc233e86386f06d4b08f4e14c0f0a8a95a
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-23 18:54:16 +0500

    to be more functional 😁

--- loops/for.py
+++ loops/for.py
@@ -1,6 +1,14 @@
-while True:
-    n = int(input("What's n?: "))
-    if n > 0:
-        break
-for _ in range(n):
-    print('meow')
+def main():
+    meow(get_number())
+def get_number():
+    while True:
+        n = int(input('What is n?: '))
+        if n > 0:
+            break
+    return n
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+main()
