410e98316bda5b451a53e8cf132cea2e52c3837f
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-23 20:05:15 +0500

    one way to get each element in an array

---
+++ loops/lists/hogwarts.py
@@ -0,0 +1,5 @@
+students = ['Hermione', 'Harry', 'Ron']
+for student in students: # in js we had to initialize
+    print(f'{student}') # we don't use underscore because we are using this variable
+
+
