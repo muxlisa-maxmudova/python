22bbc984ab1d6b74326c4a8f0a1bf91e3a6d9e42
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-23 20:32:42 +0500

    accessing keys with for loop (hogwarts students)

---
+++ loops/dictionaries/hogwarts.py
@@ -0,0 +1,11 @@
+students = {
+    'Hermione': 'Gryffindor',
+    'Harry': 'Gryffindor',
+    'Ron': 'Gryffindor',
+    'Draco': 'Slytherin'
+}
+def main():
+    for name in students:
+        print(f'{name} lives in {students[name]}')
+    print('That is all')
+main()
