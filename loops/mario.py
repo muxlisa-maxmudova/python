bf1185b9b947ff7a140a239f0fe329ce0be5e1cf
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-25 06:47:43 +0500

    creating hash bricks using just print(*)

--- loops/mario.py
+++ loops/mario.py
@@ -1,10 +1,8 @@
 # game
 def main():
-    print_column(1)
+    print_column(3)
 
 def print_column(height):
-    for _ in range(height):
-        print("#")
-
+   print("#\n" * height, end="")
 
 main()
