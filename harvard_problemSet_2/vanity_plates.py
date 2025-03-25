9ff2dd5a4696dd926b2954d99c1af29b3c109004
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-26 00:27:51 +0500

    vanity_plates worked

---
+++ harvard_problemSet_2/vanity_plates.py
@@ -0,0 +1,10 @@
+def main():
+    plate = input("Plate: ")
+    if (plate[0:2].isalpha() and
+            2<len(plate)<6 and
+            plate[-2:].isdigit() and
+            plate[-2]!='0'):
+        print("Valid")
+    elif plate.find(' 'or '.' or ','):
+        print("Invalid")
+main()
--- projects/string_methods.py
+++ projects/string_methods.py
@@ -1,12 +1,12 @@
-# name = input('Enter your full name: ')
-phone_number = input('Enter your phone number: ')
+name = input('Enter your full name: ')
+# phone_number = input('Enter your phone number: ')
 # result=len(name)
 # result = name.rfind("q")
 # name = name.capitalize() # in js we used to use toUpperCase
 # name = name.upper()
 # name = name.lower()
-# result = name.isdigit() # only returns true if only numbers
+result = name.isdigit() # only returns true if only numbers
 # result = name.isalpha() # true => only letters
 # result = phone_number.count('-') # counts how many dashes are there
-result = phone_number.replace('-', '')
+# result = phone_number.replace('-', '')
 print(result)
