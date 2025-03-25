704cd9e7a50934af3f081757054dfc3167c06609
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-25 14:31:17 +0500

    1. Learned string methods, 2. Example with while loop

---
+++ loops/lists/phone.py
@@ -0,0 +1,6 @@
+def main():
+    phone = '+1 617-495-1000'
+    print(phone[-4:]) # feature of python to slice strings,
+    # while in js we use phone.slice(0,3)
+
+main()
--- loops/while.py
+++ loops/while.py
@@ -1,5 +1,16 @@
-i = 0
-while i < 3:
-    print('meow')
-    i+=1 # 'i' will eventually reach 3
+# i = 0
+# while i < 3:
+#     print('meow')
+#     i+=1 # 'i' will eventually reach 3
+def main():
+        moisture = float(input('Level: '))
+        days = 0
+        print(f'Day {days}: Moisture is {moisture}%')
+        while moisture > 20:
+            moisture = float(input('Level: '))
+            days+=1
+            print(f'Day {days}: Moisture is {moisture}%')
+        print('Time to water!')
+main()
+
 
